f0102e272909e09e7abdb26e3c17b32bfc3fd897
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-11 18:39:29 +0000

    test for a postitive_or_negative() function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - function test for a postitive_or_negative()
+ * @i: value init;
+ */
+
+void main(void)
+{
+  int i;
+  i=0;
+  positive_or_negative(i);
+  return (0);
+}
