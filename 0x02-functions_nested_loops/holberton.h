91c23e8421310984e2be5e57b07562f4370a6ca9
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-11 12:37:45 +0000

    Header and Functions

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,34 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* task 0 - prints Holberton, followed by a new line */
+int _putchar(char c);
+
+/* task 1 - prints the alphabet, in lowercase, followed by a new line */
+void print_alphabet(void);
+
+/* task 2 - prints 10 times the alphabet, in lowercase, followed by a new line */
+void print_alphabet_x10(void);
+
+/* task 3 - checks for lowercase character */
+int _islower(int c);
+
+/* task 4 - checks for alphabetic character */
+int _isalpha(int c);
+
+/* task 5 - prints the sign of a number */
+int print_sign(int n);
+
+/* task 6 - computes the absolute value of an integer */
+int _abs(int);
+
+/* task 7 - that prints the last digit of a number */
+int print_last_digit(int);
+
+/* task 8 - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59. */
+void jack_bauer(void);
+
+/* task 9 - prints the 9 times table, starting with 0*/
+void times_table(void);
+
+#endif
