deb3130c54f3861051a3d46875fa42d27d1ff958
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-12 01:00:17 +0000

    that finds and prints the sum of the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,7 +10,7 @@ long int termino1, termino2, terminof, i, acum;
 termino1 = 1;
 termino2 = 2;
 acum = 0;
-for (i = 0; i < 48; i++)
+for (i = 0; i < 49; i++)
 {
 terminof = termino1 + termino2;
 termino1 = termino2;
