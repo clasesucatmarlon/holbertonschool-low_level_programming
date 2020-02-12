0d1cf0bc3a800308fa9cfe5c69750355a23dfb98
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-12 00:14:05 +0000

    list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - list all the natural numbers below 10 that are multiples of 3 or 5
+ * Return: valur zero
+ */
+int main(void)
+{
+int suma5, c5, c3, suma3, total;
+suma5 = 0;
+suma3 = 0;
+c5 = 5;
+c2 = 3;
+while (c5 < 1024)
+{
+suma5 = suma5 + c;
+c5 = c5 + 5;
+}
+while (c3 < 1024)
+{
+if (((c3 % 10) == 5) || ((c3 % 10) == 0))
+suma3 = suma3;
+else
+suma3 = suma3 + c2;
+c3 = c3 + 3;
+}
+total = suma5 + suma3;
+printf("%d\n", total);
+return (0);
+}
