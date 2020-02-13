f268ebd62fe7ff810e21891ef04f6f66863d3252
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-13 19:41:47 +0000

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,15 @@ int main(void)
 int i;
 for (i = 1; i < 101; i++)
 {
-if (i % 3 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
 {
 printf("Fizz ");
 }
-if (i % 5 == 0)
+else if (i % 5 == 0)
 {
 if (i == 100)
 {
@@ -24,11 +28,7 @@ else
 printf("Buzz ");
 }
 }
-if ((i % 3 == 0) && (i % 5 == 0))
-{
-printf("FizzBuzz ");
-}
-if (!(i % 3 == 0 || i % 5 == 0))
+else if (!(i % 3 == 0 || i % 5 == 0))
 {
 printf("%d ", i);
 }
