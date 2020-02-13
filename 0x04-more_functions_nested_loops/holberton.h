212c2049d44bff113c49a7c8d15bc029577effa8
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-13 17:49:33 +0000

    Header Function

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,32 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Function - _putchar */
+int _putchar(char c);
+
+/* task 0 - checks for uppercase character */
+int _isupper(int c);
+
+/* task 1 - checks for a digit (0 through 9) */
+int _isdigit(int c);
+
+/* task 2 - multiplies two integers */
+int mul(int a, int b);
+
+/* task 3 - prints the numbers from 0 to 9 */
+void print_numbers(void);
+
+/* task 4 - prints the numbers, from 0 to 9 */
+void print_most_numbers(void);
+
+/* task 5 - prints 10 times the numbers, from 0 to 14 */
+void more_numbers(void);
+
+/* task 6 - draws a straight line in the terminal */
+void print_line(int n);
+
+/* task 7 - draws a diagonal line on the terminal */
+void print_diagonal(int n);
+
+
+#endif
