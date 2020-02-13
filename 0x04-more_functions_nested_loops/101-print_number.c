cbb5fdfde398406a3e04ce150f3193422d0aa6ce
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-13 23:36:55 +0000

    function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_number - prints an integer
+ *@n: input value
+ * Return: 0
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar(45);
+n = n * (-1);
+}
+if (n / 10)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + 48);
+}
