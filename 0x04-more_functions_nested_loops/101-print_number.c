953e579c67bada38533723a41d6c30892a100c3e
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-13 23:53:08 +0000

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,15 @@
  */
 void print_number(int n)
 {
+unsigned int value = n;
 if (n < 0)
 {
 _putchar(45);
-n = n * (-1);
+value = value * (-1);
 }
-if (n / 10)
+if (value / 10)
 {
-print_number(n / 10);
+print_number(value / 10);
 }
-_putchar((n % 10) + 48);
+_putchar((value % 10) + 48);
 }
