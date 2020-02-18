a0fa2f59f5d5b791773dd4c4062537f0f1908089
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-18 14:40:35 +0000

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,18 +10,15 @@ void rev_string(char *s)
 {
 
 	int i, j, m, b;
-	char aux[10];
+	char aux[9];
 
 	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-
-	/*_putchar((char)(i + 48));*/
-
-	m = 0;
 	i--;
+	m = 0;
 	for (j = i;  s[j] > -1; j--)
 	{
 		aux[m] = s[j];
