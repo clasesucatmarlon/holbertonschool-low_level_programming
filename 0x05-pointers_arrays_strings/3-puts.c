78bdcba580aca1a9b9c647a912967441aff7870c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-18 13:18:06 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * void _puts - prints a string, followed by a new line, to stdout
+ * @str: value input
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
