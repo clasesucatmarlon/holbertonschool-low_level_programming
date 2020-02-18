df5d46414ce5e36836ec4e20ec96ea05c1fe511c
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-18 13:53:17 +0000

    prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_rev - Print reverse
+ * @s: value input
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i; j != -1; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
