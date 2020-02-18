5af33291a2c339f0c88b3eb9e360911574681b6b
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-18 17:44:27 +0000

    prints half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,23 +14,18 @@ void puts_half(char *str)
 	{
 		i++;
 	}
-	_putchar((char)(i + 48));
-	_putchar('\n');
-
 	if (i % 2 == 0)
 	{
 		mid = i / 2;
 	}
 	else
 	{
-		mid = (i - 1) / 2;
+		mid = (i + 1) / 2;
 	}
-	for (j = mid; j < i; j++)
+
+	for (j = mid - 1; j < i; j++)
 	{
 		_putchar(str[j]);
 	}
 	_putchar('\n');
-
-
-
 }
