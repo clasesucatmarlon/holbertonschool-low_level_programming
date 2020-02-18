ddab98ec2ac581e5b0380014ccc9625bb14c64ec
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-18 13:10:49 +0000

    Prtotype Functions

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,26 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Print character */
+int _putchar(char c);
+
+/* task 0 - takes a pointer to an int as parameter and updates the value it points to to 98 */
+void reset_to_98(int *n);
+
+/* task 1 - Change two numbers */
+void swap_int(int *a, int *b);
+
+/* task 2 - returns the length of a string */
+int _strlen(char *s);
+
+/* task 3 - prints a string, followed by a new line, to stdout */
+void _puts(char *str);
+
+/* task 4 - prints a string, in reverse */
+void print_rev(char *s);
+
+/* task 5 - reverses a string */
+void rev_string(char *s);
+
+
+#endif
