2d30e93632e55f87d3af58105279d0418f912e84
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-20 19:45:36 +0000

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string
+ * @dest: input value pointer
+ * @src: input value pointer
+ * @n: input value
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
