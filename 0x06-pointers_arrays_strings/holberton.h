547bb15644aba398575d444d752177e551d93f7d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-20 17:12:32 +0000

    Prototype functions

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,8 +1,11 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 
-/* task 0 - concatenates two strings */ 
+/* task 0 - concatenates two strings */
 char *_strcat(char *dest, char *src);
+
+/* task 1 - concatenates two strings */
+char *_strncat(char *dest, char *src, int n);
 
 
 #endif
