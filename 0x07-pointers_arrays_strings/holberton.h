1890a38dacb1403260ff647d61611fc59ab4b564
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-24 15:01:20 +0000

    Prototype Functions

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Function Putchar */
+int _putchar(char c);
+
+/* task 0 - fills memory with a constant byte */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
