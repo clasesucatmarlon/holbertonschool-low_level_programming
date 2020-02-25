ddba60f017b00a1d2188e64be9ccc45471067fea
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-25 03:07:30 +0000

    Prototype Functios

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -16,5 +16,19 @@ char *_strchr(char *s, char c);
 /* task 3 - gets the length of a prefix substring */
 unsigned int _strspn(char *s, char *accept);
 
+/* task 4 - searches a string for any of a set of bytes */
+char *_strpbrk(char *s, char *accept);
+
+/* task 5 - locates a substring */
+char *_strstr(char *haystack, char *needle);
+
+/* task 7 - prints the chessboard */
+void print_chessboard(char (*a)[8]);
+
+/* task 8 - prints the sum of the two diagonals of a square matrix of integers */
+void print_diagsums(int *a, int size);
+
+/* task 9 - sets the value of a pointer to a char. */
+void set_string(char **s, char *to);
 
 #endif
