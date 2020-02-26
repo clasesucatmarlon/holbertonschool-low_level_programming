0c3446abb8fe6a459236b3182421268e2fc62ad9
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-26 17:27:30 +0000

    Prototype Functions

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Function Putchar */
+int _putchar(char c);
+
+/* task 0 - prints a string */
+void _puts_recursion(char *s);
+
+/*task 2 - prints a string in reverse.*/
+void _print_rev_recursion(char *s);
+
+#endif
