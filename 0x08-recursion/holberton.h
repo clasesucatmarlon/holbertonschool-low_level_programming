d08a119634d98c1ec82ea87f6c9d95713de75e09
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-27 00:11:59 +0000

    Prototype Functions

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -22,7 +22,10 @@ int _pow_recursion(int x, int y);
 /* task 5 - returns the natural square root of a number */
 int _sqrt_recursion(int n);
 
-/* tas6 - Print number prime */
+/* task 6 - Print number prime */
 int is_prime_number(int n);
+
+/* task 7 - 1 if a string is a palindrome and 0 if not */
+int is_palindrome(char *s);
 
 #endif
