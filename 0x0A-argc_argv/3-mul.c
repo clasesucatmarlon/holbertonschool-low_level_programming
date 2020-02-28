e7779cde5b71ae39cf1ed43ae6e5c00f1ff5953d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-02-28 23:19:45 +0000

    mult two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - add two numbers
+ * @argc: parameters values
+ * @argv: array
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int mult;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		int a = atoi(argv[1]);
+		int b = atoi(argv[2]);
+
+		mult = a * b;
+		printf("%d\n", mult);
+	}
+	return (0);
+
+}
