7641e2eb8df48bbb3adb6f9eda6c1eb1822dd8b6
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-03 19:31:16 +0000

    Prototype Functions

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -7,5 +7,8 @@ char *create_array(unsigned int size, char c);
 /* task 1 - eturns a pointer to a newly allocated */
 char *_strdup(char *str);
 
+/* task 2 - concatened two string */
+char *str_concat(char *s1, char *s2);
+
 
 #endif
