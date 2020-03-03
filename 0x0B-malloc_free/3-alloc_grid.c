78890cde4cd51bb568d011cde41d5c5c933ab185
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-03 21:26:14 +0000

    print array two dimensions

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - eturns a ptr to a 2 dimen array of int
+ * @width: input value int
+ * @height: input value int
+ * Return: int
+ */
+
+int **alloc_grid(int width, int height)
+{
+
+	int **w;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	w = malloc(height * sizeof(int));
+
+	for (i = 0; i < height; i++)
+	{
+		w[i] = malloc(width * sizeof(int));
+		for (j = 0; j < height; j++)
+		{
+			w[i][j]	= 0;
+		}
+	}
+	return (w);
+}
+
+
+
+
