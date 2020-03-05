b680aa201b33d1a39e5b93ae2054c05915d21158
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-05 14:42:57 +0000

    Prototype Functions

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* task 0 - allocates memory using malloc */
+void *malloc_checked(unsigned int b);
+
+/* task 1 - concatenates two strings */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+/* task 2 - allocates memory for an array, using malloc */
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+/* task 3 - creates an array of integers */
+int *array_range(int min, int max);
+
+
+#endif
