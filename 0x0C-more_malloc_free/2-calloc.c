936aa86b1a7446d4102190a61e034b4a5803b9de
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-05 18:57:29 +0000

    allocate memory using malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _calloc - calloc
+ * @nmemb: value first
+ * @size: size value
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	unsigned int *x;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	x = malloc(nmemb * sizeof(unsigned int));
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		x[i] = 0;
+	}
+
+	return (x);
+
+}
