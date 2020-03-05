c1ee947c1309e56044738c619d302503bf383de4
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-05 17:14:57 +0000

    asigne memoria usando malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocated memory used malloc
+ * @b: input value unsigned int
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	unsigned int *x;
+
+	x = (unsigned int*)malloc(b * sizeof(unsigned int));
+	if (x == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return ();
+	}
+
+}
