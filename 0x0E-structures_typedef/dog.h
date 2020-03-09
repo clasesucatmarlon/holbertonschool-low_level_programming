b30979df64a0d747889eb47ac877a6642ccfb1a6
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-09 21:06:43 +0000

    Prototype Functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -21,4 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 /* task 2 - prints a struct dog */
 void print_dog(struct dog *d);
 
+/* task 4 - creates a new dog */
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
