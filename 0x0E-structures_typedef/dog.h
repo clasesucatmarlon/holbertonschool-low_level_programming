bbb34f5239f0b4393b4469cc43f36670df3268a0
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-09 20:53:34 +0000

    Prototype Functions

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 /* task 1 - nitialize a variable of type struct dog */
 void init_dog(struct dog *d, char *name, float age, char *owner);
