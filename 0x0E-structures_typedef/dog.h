f9e80df6fe6b87c7defe18a0278c8e9cfabc7675
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-09 18:16:53 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+/**
+ * struct dog - struct dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: structure of dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
