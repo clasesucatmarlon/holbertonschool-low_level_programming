315e2d8a8ee585f273cbd899385afe0d6f7f2f3e
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-11 15:04:07 +0000

    Prototype Functions

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* task 0 - print name */
+void print_name(char *name, void (*f)(char *));
+
+/* task 1 - executes a function given as a parameter
+on each element of an array */
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* task 2 - searches for an integer */ 
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
