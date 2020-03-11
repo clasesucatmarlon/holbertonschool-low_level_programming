e35256aed00fa8231531d42903b3772064f16782
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-11 17:49:33 +0000

    prototype Functions

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,9 +4,8 @@
 /* task 0 - print name */
 void print_name(char *name, void (*f)(char *));
 
-/* task 1 - executes a function given as a parameter
-on each element of an array */
-/*void array_iterator(int *array, size_t size, void (*action)(int));*/
+/* task 1 - executes a function given as a parameter */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 /* task 2 - searches for an integer */
 int int_index(int *array, int size, int (*cmp)(int));
