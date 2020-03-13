936590823f6c29ce5c5486ff58d133c8a4f194d0
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-13 17:28:48 +0000

    add elements

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function sum elements
+ * @n: elements of arguments
+ * Return: int
+ */
+
+ int sum_them_all(const unsigned int n, ...)
+ {
+
+	va_list alist;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(alist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(alist, unsigned int);
+	}
+
+	va_end(alist);
+	return (sum);
+
+ }
+ 
