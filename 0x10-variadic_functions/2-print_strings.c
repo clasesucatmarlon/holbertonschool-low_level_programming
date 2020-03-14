60121c24f027eb86060e592ac50465528aee6457
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-03-14 18:02:06 +0000

    print string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -27,7 +27,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (i < n - 1)
 			{
 				if (separator != NULL)
-					printf("%s%s", p, separator);
+					printf("%s", separator);
+				printf("%s", p);
 			}
 			else
 			{
