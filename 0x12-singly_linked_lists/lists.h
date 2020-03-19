8f70cdbf09ccbd69eec766f2ef2d5a7f97e2c7a8
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-19 22:20:00 +0000

    Prototype Functions of List

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -43,7 +43,8 @@ list_t *add_node(list_t **head, const char *str);
 /* task 3 - adds a new node at the end of a list_t list */
 list_t *add_node_end(list_t **head, const char *str);
 
-/* task 4 */
+/* task 4 - frees a list_t list*/
+void free_list(list_t *head);
 
 
 #endif
