b12e739ccce5c7d33c9e073da346f2b00192bcfb
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-19 18:34:22 +0000

    Prototype Functions of List

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -31,5 +31,8 @@ size_t print_list(const list_t *h);
 /* task 1 -  returns the number of elements in a linked list_t list */
 size_t list_len(const list_t *h);
 
+/* task 2 - adds a new node at the beginning of a list_t list */
+list_t *add_node(list_t **head, const char *str);
+
 
 #endif
