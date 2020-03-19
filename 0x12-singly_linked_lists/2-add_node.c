8192e71f0169dd72d2767440a087c70b48bf350b
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-19 20:05:27 +0000

    adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * add_node - Creates a node and adds it to a linked list
+ * @head: Pointer to head of the linked list
+ * @str: String to assigned to the node
+ * Return: Pointer to the head of the linked list
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	if (head == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+	
+	/*Initialize the next pointer witch NULL*/
+	if (*head == NULL) 
+		newNode->next = NULL;
+	else
+		/*Asigned head to newNode*/
+		newNode->next = *head;
+	newNode->str = strdup(str);
+	newNode->len = large(str);
+	*head = newNode;
+	return (*head);
+}
+
+int large(const char *str)
+{
+
+	int l;
+
+	if (str == NULL)
+		return (0);
+
+    while (str[l] != '\0')
+    {
+        l++;
+    }
+
+	return (l);
+}
