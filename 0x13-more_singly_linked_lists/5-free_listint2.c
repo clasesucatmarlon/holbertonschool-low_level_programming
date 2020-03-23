bf33bca5be8559a13a5bb1af133cfb629f1a770b
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-23 16:29:41 +0000

    frees a listint_t list, two pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * free_listint2 - Frees a list of nodes, and sets the head to NULL
+ * @head: Pointer to the first element of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
+
+	while ((*head)->next != NULL)
+	{
+		aux = (*head)->next;
+		free(*head);
+		*head = aux;
+	}
+	free(*head);
+	*head = NULL;
+}
