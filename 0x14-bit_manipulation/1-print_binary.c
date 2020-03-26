c3a22e4da8ffb38cf4627135eb43bade7b4a2daf
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-26 20:26:50 +0000

    print number to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_binary - print number to binary
+ * @n: numbers base 10
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+
+
+	if (n == 0 || n == 1)
+	{
+		_putchar(n + '0');
+		return;
+	}
+
+	print_binary(n >> 1);
+
+	_putchar((n - ((n >> 1) * 2)) + '0');
+
+}
