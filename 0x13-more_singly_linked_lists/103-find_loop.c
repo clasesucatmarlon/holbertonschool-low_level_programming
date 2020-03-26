a7934067e6ada11ff0c20ac520abb1ed88ca068e
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-26 00:21:06 +0000

    finds the loop in a linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * find_listint_loop - finds the loop inside of a linked list
+ * @head: start of linked list
+ * Return: address of node where loop begins
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *aux = head, *control;
+
+	if (!head)
+		return (NULL);
+
+	if (head == head->next)
+		return (head);
+
+	while (aux)
+	{
+		aux = aux->next;
+		control = head;
+		while (aux && control != aux)
+		{
+			if (control == aux->next)
+			return (control);
+			control = control->next;
+		}
+	}
+	return (aux);
+}
