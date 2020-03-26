0b7185143249eefe087644e4226d864897d388c0
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-26 17:45:45 +0000

    converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - Reverses a singly linked list
+ * @b: pointer to the first element of the reversed list
+ * Return: A pointer to the first element of the reversed list
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int i = 0;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '1' && b[i] != '0')
+		{
+			return (0);
+		}
+
+		sum <<= 1;
+		if (b[i] == '1')
+		{
+			sum = sum + 1;
+		}
+
+		i++;
+	}
+	return (sum);
+}
