9e529433248ea9b1825122684658d26de37a26ed
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-31 19:45:32 +0000

    Prototypes Functions

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -12,6 +12,8 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /* task 1 - create file */
 int create_file(const char *filename, char *text_content);
 
+/* task 2 - append text to file */
+int append_text_to_file(const char *filename, char *text_content);
 
 
 #endif
