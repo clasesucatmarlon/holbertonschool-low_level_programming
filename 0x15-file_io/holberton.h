1576806c6f2ecb16dac7223ea26ea9b5e0376e2b
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-31 19:21:27 +0000

    Prototype Functions

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -9,6 +9,9 @@
 /* task 0 - reads a text file and prints it to the POSIX standard output */
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/* task 1 - create file */
+int create_file(const char *filename, char *text_content);
+
 
 
 #endif
