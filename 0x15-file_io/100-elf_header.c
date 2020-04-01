ced147226e2f5ae5085a9f291fc097b7616f73e2
clasesucatmarlon <clasesucatmarlon@github.com>
2020-04-01 02:56:57 +0000

    program that displays the information contained in the ELF header at the start of an ELF file

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * @argc: Count of the arguments to start the program
+ * @argv: An array of strings containing the arguments passed to the program
+ * Return: zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int openFile, readFile;
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename");
+		EXIT(98);
+	}
+	openFile = open(argv[1], O_RDONLY);
+	if (openFile == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+}
