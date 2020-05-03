f6374d565d27c70129c11b0d8f76bd9b126a30b2
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-03 15:32:21 +0000

    inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,13 +4,13 @@
  * insert_dnodeint_at_index - insert node in idx
  * @h: head of nodes
  * @idx: index to insert nodes
- * @n: data to insert in node_new node
- * Return: list with node_new node
+ * @n: data to insert in node_node_new node
+ * Return: list with node_node_new node
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int count;
-	dlistint_t *new, *runner;
+	dlistint_t *node_new, *node_tmp;
 
 	if (h == NULL)
 		return (NULL);
@@ -19,29 +19,29 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 
 	count = 0;
-	runner = *h;
+	node_tmp = *h;
 	while (count < idx - 1)
 	{
-		if (runner == NULL)
+		if (node_tmp == NULL)
 			return (NULL);
 		count++;
-		runner = runner->next;
+		node_tmp = node_tmp->next;
 	}
-	if (runner->next == NULL)
+	if (node_tmp->next == NULL)
 	{
 		return (add_dnodeint_end(h, n));
 	}
 
 	else
 	{
-		new = malloc(sizeof(dlistint_t));
-		if (!new)
+		node_new = malloc(sizeof(dlistint_t));
+		if (!node_new)
 			return (NULL);
-		new->n = n;
-		new->next = runner->next;
-		new->next->prev = new;
-		runner->next = new;
-		new->prev = runner;
+		node_new->n = n;
+		node_new->next = node_tmp->next;
+		node_new->next->prev = node_new;
+		node_tmp->next = node_new;
+		node_new->prev = node_tmp;
 	}
-	return (new);
+	return (node_new);
 }
