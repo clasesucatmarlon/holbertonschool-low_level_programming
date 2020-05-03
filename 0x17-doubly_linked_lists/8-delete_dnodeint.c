acf501126f8e7f83fee8bf17e6db5e9abe9d21ff
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-03 15:42:38 +0000

    deletes the node at index index of a dlistint_t linked list

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,40 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node at index
- * @head: double pointer to head of nodes
- * @index: position of index to deete
- * Return: list with deleted node
+ * delete_node - deletes a node from a linked list
+ * @head: head of the list
+ * @del_node: node to be deleted
+ */
+void delete_node(dlistint_t **head, dlistint_t *del_node)
+{
+	if (*head == del_node)
+		*head = del_node->next;
+	if (del_node->next != NULL)
+		del_node->next->prev = del_node->prev;
+	if (del_node->prev != NULL)
+		del_node->prev->next = del_node->next;
+	free(del_node);
+}
+
+/**
+ * delete_dnodeint_at_index - deletes a node at a given index
+ * @head: head of the linked list
+ * @index: index of node to delete
+ * Return: returns 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp_node = NULL;
-	unsigned int count = 0;
+	unsigned int count;
+	dlistint_t *node_tmp;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (-1);
 
-	tmp_node = *head;
+	for (count = 0, node_tmp = *head; node_tmp != NULL && count < index; count++)
+		node_tmp = node_tmp->next;
+	if (node_tmp == NULL)
+		return (-1);
 
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
-		free(tmp_node);
-		return (1);
-	}
-
-	while (tmp_node->next != NULL)
-	{
-		if (count == index)
-		{
-			tmp_node->next->prev = tmp_node->prev;
-			tmp_node->prev->next = tmp_node->next;
-			free(tmp_node);
-			return (1);
-		}
-		tmp_node = tmp_node->next;
-		count++;
-	}
-	return (-1);
+	delete_node(head, node_tmp);
+	return (1);
 }
