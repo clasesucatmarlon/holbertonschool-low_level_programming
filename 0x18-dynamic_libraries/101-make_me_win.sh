5c83fbfeb3c271a4f967b8e31a44df1b265bdabb
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-05-07 00:02:16 +0000

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/clasesucatmarlon/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/library.so
+wget -P /tmp https://raw.githubusercontent.com/clasesucatmarlon/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/library.so
 export LD_PRELOAD=/tmp/library.so
