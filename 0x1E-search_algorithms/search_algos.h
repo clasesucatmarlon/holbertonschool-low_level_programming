eef8b5ece5bf191f30bb330b54754ab2041cda9d
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-26 12:15:32 -0500

    search_algos.h added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 
 void func_print_array(int const *left, int const *right);
