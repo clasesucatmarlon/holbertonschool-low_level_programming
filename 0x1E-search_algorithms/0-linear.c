49e0092740144848ef5a8a3400b18b03bfd72755
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-26 12:45:38 -0500

    Task: 0. Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
 /**
  * linear_search - search linear
  * @array: pointer to first element to array
- * @size : numbers of items in the array
+ * @size: numbers of items in the array
  * @value: value to serch
  * Return: index of value in array or -i if not found
  */
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (!array | size == 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
