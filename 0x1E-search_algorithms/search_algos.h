bce0604e11759f9f9cbeb13bf04d14a28b34d705
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-26 12:31:08 -0500

    search_algos.h added

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,4 +13,5 @@ int jump_search(int *array, size_t size, int value);
 
 
 void func_print_array(int const *left, int const *right);
+int jump(int *array, int start, int end, int size, int value);
 #endif /* SEARCH_ALGOS_H */
