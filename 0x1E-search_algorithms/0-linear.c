87a91e1cdbef92a187c0608eae0b1004ceb0fc2a
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-26 12:26:13 -0500

    Task: 0. Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,18 +11,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned long int i;
+	unsigned int i;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
-
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
 		if (array[i] == value)
-		{
 			return (i);
-		}
 	}
 	return (-1);
 }
